32bf24665741e4e836ab0acdb06926e2c67e6ed3
robertocajueiro <beto.cajueiro@gmail.com>
2023-02-20 22:16:59 -0300

    22.8. Criando o DTO do relatório

--- src/main/java/com/algaworks/algamoney/api/model/TipoLancamento.java
+++ src/main/java/com/algaworks/algamoney/api/model/TipoLancamento.java
@@ -2,7 +2,21 @@ package com.algaworks.algamoney.api.model;
 
 public enum TipoLancamento {
 	
-	RECEITA,
-	DESPESA
+	RECEITA("Receita"),
+	DESPESA("Despesa")
+	
+	;
+	
+	private final String descricao;
+	
+	TipoLancamento(String descricao){
+		this.descricao = descricao;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	
 
 }
