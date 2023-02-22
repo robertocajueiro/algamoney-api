73c2f04ef008ca6bf0bf7a35a25158d4c9233aa4
robertocajueiro <beto.cajueiro@gmail.com>
2023-02-21 22:23:05 -0300

    22.15. Criando um agendamento de tarefa (Scheduler)

--- src/main/java/com/algaworks/algamoney/api/service/LancamentoService.java
+++ src/main/java/com/algaworks/algamoney/api/service/LancamentoService.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import com.algaworks.algamoney.api.dto.LancamentoEstatisticaPessoa;
@@ -33,6 +34,11 @@ public class LancamentoService {
 	
 	@Autowired
 	private LancamentoRepository lancamentoRepository;
+	
+	@Scheduled(cron = "0 19 22 * * *")
+	public void avisarSobreLancamentosVencidos() {
+		System.out.println(">>>>>>>>>>>>>>Método sendo executado....<<<<<<<<<<<<<<<<<<");
+	}
 	
 	public byte[] relatorioPorPessoa(LocalDate inicio, LocalDate fim) throws Exception {
 		List<LancamentoEstatisticaPessoa> dados = lancamentoRepository.porPessoa(inicio, fim);
