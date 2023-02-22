ba7591bd75c393648ab9d6c896866c7c2c69906f
robertocajueiro <beto.cajueiro@gmail.com>
2023-02-22 17:38:31 -0300

    22.16. Configurando o envio de e-mail

--- src/main/java/com/algaworks/algamoney/api/config/property/AlgamoneyApiProperty.java
+++ src/main/java/com/algaworks/algamoney/api/config/property/AlgamoneyApiProperty.java
@@ -10,6 +10,12 @@ public class AlgamoneyApiProperty {
 	private String originPermitida = "http://localhost:8000";
 
 	private final Seguranca seguranca = new Seguranca();
+	
+	private final Mail mail = new Mail();
+	
+	public Mail getMail() {
+		return mail;
+	}
 
 	public Seguranca getSeguranca() {
 		return seguranca;
@@ -36,4 +42,38 @@ public class AlgamoneyApiProperty {
 		}
 
 	}
+	
+	public static class Mail {
+		private String host;
+		private Integer port;
+		private String username;
+		
+		public String getHost() {
+			return host;
+		}
+		public void setHost(String host) {
+			this.host = host;
+		}
+		public Integer getPort() {
+			return port;
+		}
+		public void setPort(Integer port) {
+			this.port = port;
+		}
+		public String getUsername() {
+			return username;
+		}
+		public void setUsername(String username) {
+			this.username = username;
+		}
+		public String getPassword() {
+			return password;
+		}
+		public void setPassword(String password) {
+			this.password = password;
+		}
+		private String password;
+	}
+	
+	
 }
